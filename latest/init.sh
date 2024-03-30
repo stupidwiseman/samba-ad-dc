41e91ddb10e137bcd1246f6d51943f2a56d38642
Stupid Wiseman <stupidwiseman@outlook.jp>
2024-03-30 10:37:26 +0900

    smb.confの削除プロセスを一部修正

--- latest/init.sh
+++ latest/init.sh
@@ -3,11 +3,13 @@
 # 変数定義
 ADSSETUP=${ADSSETUP:-PRIMARY}
 HOST_IP=${HOST_IP:-na}
+UPASSWD=${UPASSWD:-$ADPASSWD}
 
 if [ $ADSSETUP == "PRIMARY"]; then
     DOMAIN="--domain=${DOMAIN}"
     REALM="--realm=${REALM}"
-    DPASSWD="--adminpass=${DPASSWD}"
+    ADPASSWD="--adminpass=${ADPASSWD}"
+    UPASSWD="--password=${UPASSWD}"
     if [ HOST_IP != "na" ]; then
         HOST_IP="--host-ip=${HOST_IP}"
     fi
@@ -15,20 +17,19 @@ fi
 
 # ADのセットアップ内容に基づくコマンドオプションセット
 if [ $ADSSETUP == "PRIMARY"]; then
-    opt1="domain provision --use-rfc2307 ${DOMAIN} ${REALM} --server-role=dc --dns-backend=SAMBA_INTERNAL ${DPASSWD} ${HOST_IP}"
-    elif [ $ADSSETUP == "SECONDARY" ]; then
-        opt1="domain join ${REALM} DC -U ${ADMIN} --password=${DPASSWD}"
+    if [ -e /etc/samba/smb.conf ]; then
+        rm /etc/samba/smb.conf
+    fi
+    opt1="domain provision --use-rfc2307 ${DOMAIN} ${REALM} --server-role=dc --dns-backend=SAMBA_INTERNAL ${ADPASSWD} ${HOST_IP}"
+    else
+        opt1="domain join ${REALM} DC -U ${ADMIN} ${UPASSWD}"
         opt2="drs replicate ${PDC} ${SDC} ${REALM}"
 fi
 
 # ADのセットアップ内容に基づく初期設定
-if [ ${ADSSETUP} == "PRIMARY" ]; then
-    rm /etc/samba/smb.conf
-fi
 samba-tool ${opt1}
 service samba-ad-dc restart
 
 if [ ${ADSSETUP} == "SECONDARY" ]; then
-    net ads join ${REALM} -U 
     samba-tool ${opt2}
 fi
