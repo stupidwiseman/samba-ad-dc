bac7bd58c5c3b586c8b3860bc4f28bda422a72c2
Stupid Wiseman <stupidwiseman@outlook.jp>
2024-03-30 09:58:52 +0900

    ドメインログイン処理追加

--- latest/init.sh
+++ latest/init.sh
@@ -17,14 +17,18 @@ fi
 if [ $ADSSETUP == "PRIMARY"]; then
     opt1="domain provision --use-rfc2307 ${DOMAIN} ${REALM} --server-role=dc --dns-backend=SAMBA_INTERNAL ${DPASSWD} ${HOST_IP}"
     elif [ $ADSSETUP == "SECONDARY" ]; then
-        opt1="domain join ${REALM} DC -U ${ADMIN} --realm=${REALM}"
+        opt1="domain join ${REALM} DC -U ${ADMIN} --password=${DPASSWD}"
         opt2="drs replicate ${PDC} ${SDC} ${REALM}"
 fi
 
 # ADのセットアップ内容に基づく初期設定
+if [ ${ADSSETUP} == "PRIMARY" ]; then
+    rm /etc/samba/smb.conf
+fi
 samba-tool ${opt1}
 service samba-ad-dc restart
 
 if [ ${ADSSETUP} == "SECONDARY" ]; then
+    net ads join ${REALM} -U 
     samba-tool ${opt2}
 fi
